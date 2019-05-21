2dabe5f3da12e810501fbe386e82c21373073d3d
trvadim <49869514+trvadim@users.noreply.github.com>
2019-05-21 11:28:51 +0300

    init project (#4)

--- src/main.js
+++ src/main.js
@@ -1,42 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import VueRouter from "vue-router";
-import App from "./App";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
 
-// router setup
-import routes from "./routes/routes";
+Vue.config.productionTip = false
 
-// Plugins
-import GlobalComponents from "./globalComponents";
-import GlobalDirectives from "./globalDirectives";
-import Notifications from "./components/NotificationPlugin";
-
-// MaterialDashboard plugin
-import MaterialDashboard from "./material-dashboard";
-
-import Chartist from "chartist";
-
-// configure router
-const router = new VueRouter({
-  routes, // short for routes: routes
-  linkExactActiveClass: "nav-item active"
-});
-
-Vue.prototype.$Chartist = Chartist;
-
-Vue.use(VueRouter);
-Vue.use(MaterialDashboard);
-Vue.use(GlobalComponents);
-Vue.use(GlobalDirectives);
-Vue.use(Notifications);
-
-/* eslint-disable no-new */
 new Vue({
-  el: "#app",
-  render: h => h(App),
   router,
-  data: {
-    Chartist: Chartist
-  }
-});
+  store,
+  render: h => h(App)
+}).$mount('#app')

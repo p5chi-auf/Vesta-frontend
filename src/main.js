5721b75df6268ca16df81999c8498a10bfb13798
Vasile Grinciuc <vgrinciuc@straighterline.com>
2019-05-21 13:38:30 +0300

    Merge

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import router from "@/router";
+import "@/plugins";
+import store from "@/store";
 
 Vue.config.productionTip = false;
 
---
+++ src/router/auth.js
@@ -0,0 +1,8 @@
+import Login from "@/components/Layouts/Auth/Login";
+export default [
+  {
+    path: "login",
+    name: "login",
+    component: Login
+  }
+];

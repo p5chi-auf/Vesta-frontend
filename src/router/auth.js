ef477a7d5dc77b27d496ded5e97ca6aac00627b8
trvadim <49869514+trvadim@users.noreply.github.com>
2019-05-30 15:34:51 +0300

    Adding the registration page (#8)

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,8 +1,14 @@
 import Login from "@/components/Layouts/Auth/Login";
+import Register from "@/components/Layouts/Auth/Register";
 export default [
   {
     path: "login",
     name: "login",
     component: Login
+  },
+  {
+    path: "register",
+    name: "register",
+    component: Register
   }
 ];

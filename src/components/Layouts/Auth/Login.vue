c7e1633755ce53078ba3f9d8fa1ee9a574273cc0
vtrifan <vtrifan@pentalog.com>
2019-06-05 13:53:57 +0300

    Change Login and Register page

--- src/components/Layouts/Auth/Login.vue
+++ src/components/Layouts/Auth/Login.vue
@@ -1,39 +1,58 @@
 <template>
-  <v-flex xs12 sm8 md4>
+  <v-flex xs12 sm5 md3>
     <v-card class="elevation-12">
       <v-toolbar color="teal darken-1" dark>
         <v-toolbar-title class="ma-auto">
           Welcome
         </v-toolbar-title>
       </v-toolbar>
-      <v-card-text>
-        <v-form>
-          <v-text-field
-            prepend-icon="person"
-            name="login"
-            label="Login"
-            type="text"
-            required
-          ></v-text-field>
-          <v-text-field
-            id="password"
-            prepend-icon="lock"
-            name="password"
-            label="Enter your password"
-            type="password"
-          ></v-text-field>
-        </v-form>
-      </v-card-text>
-      <v-card-actions class="ma-4">
-        <router-link :to="{ name: 'forgotpassword' }"
-          >Forgot Password?</router-link
-        >
-        <v-layout row wrap justify-end>
-          <v-flex xs12 md2>
+      <v-flex xs10 offset-xs1>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              outline
+              name="login"
+              label="Login"
+              type="text"
+              required
+              ><v-icon>home</v-icon></v-text-field
+            >
+            <v-text-field
+              id="password"
+              outline
+              name="password"
+              label="Enter your password"
+              type="password"
+            ></v-text-field>
+            <v-checkbox
+              v-model="ex4"
+              label="Remember Me !"
+              color="info"
+              value="Remember Me !"
+              hide-details
+            ></v-checkbox>
+          </v-form>
+        </v-card-text>
+        <v-layout row justufy-center>
+          <v-flex xs10 offset-xs1>
             <v-btn block color="info">Login</v-btn>
           </v-flex>
         </v-layout>
-      </v-card-actions>
+        <v-card-actions class="ma-3">
+          <v-layout row wrap justify-space-between>
+            <v-flex xs12 md6>
+              <router-link :to="{ name: 'register' }">
+                Create Account
+              </router-link>
+            </v-flex>
+            <v-flex xs12 md4>
+              <router-link :to="{ name: 'forgotpassword' }"
+                >Forgot Password?</router-link
+              >
+            </v-flex>
+          </v-layout>
+        </v-card-actions>
+      </v-flex>
     </v-card>
   </v-flex>
 </template>

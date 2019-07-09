19186e00c11bc65d00a9e427e8a71b69eb6e0bed
vtrifan <vtrifan@pentalog.com>
2019-07-09 09:52:02 +0300

    add logout

--- src/components/Layouts/Dashboard/Header.vue
+++ src/components/Layouts/Dashboard/Header.vue
@@ -6,15 +6,23 @@
     app
     fixed
   >
+    <!--    <v-btn-icon fab>-->
+    <!--      <v-icon>-->
+    <!--        <img src="/img/registration/logo.png" />-->
+    <!--      </v-icon>-->
+    <!--    </v-btn-icon>-->
+
     <v-toolbar-title class="ml-0 pl-3 toolbar">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+
       <span class="title ml-3 mr-5">
         Vesta
-        <span class="font-weight-light">
-          Users
-        </span>
+        <!--        <span class="font-weight-light">-->
+        <!--          Users-->
+        <!--        </span>-->
       </span>
     </v-toolbar-title>
+
     <v-text-field
       flat
       solo-inverted
@@ -30,14 +38,68 @@
     <v-btn icon>
       <v-icon>notifications</v-icon>
     </v-btn>
-    <v-btn icon large>
-      <v-avatar size="42px">
-        <img src="/img/avatar/vesta.png" alt="Vesta" />
-        <!-- TODO make the logout button-->
-      </v-avatar>
-    </v-btn>
+
+    <v-menu dark sesion="1000" transition="slide-y-transition" bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn fab icon avatar v-on="on">
+          <v-avatar>
+            <img src="/img/avatar/user1.png" alt="Vesta" />
+          </v-avatar>
+        </v-btn>
+      </template>
+      <v-card>
+        <v-container>
+          <v-layout row justify-content-end> </v-layout>
+          <v-list>
+            <v-layout row>
+              <div class="mx-auto">
+                <img src="/img/avatar/user1b.png" width="130px" />
+                <v-list>
+                  <v-layout row wrap justify-space-between>
+                    <v-list-tile-title
+                      ><h2 font-weight-bold display-4>
+                        Trifan Vadim
+                      </h2></v-list-tile-title
+                    >
+                  </v-layout>
+                </v-list>
+              </div>
+            </v-layout>
+            <v-layout justify-space-between>
+              <v-btn flat color="blue">View Profile</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn flat color="red" @click="logout">Log Out</v-btn>
+            </v-layout>
+          </v-list>
+        </v-container>
+      </v-card>
+    </v-menu>
+    <!--    </v-menu>-->
   </v-toolbar>
 </template>
+<script>
+//TODO import mapMutations from vuex
+import { mapMutations } from "vuex";
+export default {
+  data: () => ({
+    menu: false,
+    drawer: true
+  }),
+  methods: {
+    // use mapMutations with 'auth/mutateTokens'
+    ...mapMutations({
+      mutateTokens: "auth/mutateTokens"
+    }),
+    logout() {
+      this.mutateTokens({});
+      this.$router.push("/auth/login");
+      //TODO call mutation with empty tokens
+      //TODO after it redirect it to auth page
+    }
+  }
+};
+</script>
+
 <style scoped>
 .toolbar {
   width: 300px;

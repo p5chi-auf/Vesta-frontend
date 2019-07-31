ee89a957d4b7b7293109d04fa9a6ca7d206db427
vtrifan <vtrifan@pentalog.com>
2019-07-31 16:56:32 +0300

    Add svg images, request from back-end (/company)

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,6 @@ import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
-
 import * as modules from "./modules";
 export default new Vuex.Store({
   modules,
